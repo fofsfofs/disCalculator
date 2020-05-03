7e5f92f4920bf3a54d6664c696f7aeb16667ce0f
Erio Hoti <erio.hoti@ryerson.ca>
2020-05-02 23:39:16 -0400

    Erio.py edit

--- cogs/Erio.py
+++ cogs/Erio.py
@@ -1,16 +1,62 @@
 import discord
 from discord.ext import commands
+from mathpix.mathpix import MathPix
+from PIL import Image
+import requests
+import wolframalpha
+
+reader = MathPix(
+    app_id="discalculator_gmail_com_f307ad", app_key="684b628a6ac77aeb3d1f"
+)
 
 
 class Erio(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    def basecalc(self,query):
+        wolfclient = wolframalpha.Client("L766YW-6V34XVRVWG")
+        res = wolfclient.query(query)
+
+        if hasattr(res, 'results'):
+            return next(res.results).text
+        else:
+            return None
+
+    def latexToImage(self, somethingelse):
+        # url = ctx.message.attachments[0].url
+        
+        formula = somethingelse.replace("\n", " ")
+        r = requests.get(
+            "http://latex.codecogs.com/png.latex?\dpi{{300}} {formula}".format(
+                formula=formula
+            )
+        )
+        f = open("example.png", "wb")
+        f.write(r.content)
+        f.close()
+        im = Image.open("example.png")
+        rgb_im = im.convert("RGB")
+        rgb_im.save("pls.jpg")
+
     @commands.command()
-    async def calc(self,ctx):
+    async def calc(self, ctx):
+        msg = ctx.message.content[5:]
         url = ctx.message.attachments[0].url
-        await ctx.send('is this your url? {}'.format(url))
+        ocr = reader.process_image(image_url=url)
+
+        Erio.latexToImage(self,ocr.latex) 
+        try:
+            await ctx.send(file=discord.File("pls.jpg"))
+            await ctx.trigger_typing()
+            result = await self.client.loop.run_in_executor(None, Erio.basecalc, self, msg + " " + ocr.latex)
+            await ctx.send(result)
+            await Erio.latexToImage(self,result)
+        except:
+            await ctx.send("Took too long! Or an unexpected error occurred.")
 
 
 def setup(client):
     client.add_cog(Erio(client))
+
+
